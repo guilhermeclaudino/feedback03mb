9be3868429a942b6fcb3d7d81396ead5a052a4ab
gui.claudino08 <gui.claudino08@gmail.com>
2025-04-22 02:12:14 +0000

    mudança da função handleMudarNota para o componente InputButton, já que é responsabilidade dele

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ export function App() {
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
   const [submited, setSubmited] = useState(false)
 
-  function handleMudarNotaAvaliacao(nota) {
-    setNotaAvaliacao(nota)
-  }
-
   function handleSubmit() {
     if (notaAvaliacao !== 0) {
       setSubmited(true)
@@ -22,7 +18,7 @@ export function App() {
 
   return (
     submited === false ? (
-      <CardInicial handleMudarNotaAvaliacao={handleMudarNotaAvaliacao} handleSubmit={handleSubmit} />
+      <CardInicial setNotaAvaliacao={setNotaAvaliacao} handleSubmit={handleSubmit} />
     ) : (
       <CardFinal notaAvaliacao={notaAvaliacao}/>
     )
